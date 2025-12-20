ac89b142246acf874c5eb5312fb90c0265653ed8
Giao Vu Dinh <tvudinh20@gmail.com>
2025-12-20 12:29:35 +0900

    type error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalForPrisma.prisma;
 }
 
 export default prisma;
