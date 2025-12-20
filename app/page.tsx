cb9dcaaddcd358bdf0b537ace05e343a3054db98
Giao Vu Dinh <tvudinh20@gmail.com>
2025-12-20 12:23:19 +0900

    setting up auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-import prisma from "@/lib/prisma";
+import UserAuth from "./components/auth/UserAuth";
 
-export default async function Home() {
-  const users = await prisma.user.findMany();
-
+export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         Superblog
       </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {users.map((user) => (
-          <li key={user.id} className="mb-2">
-            {user.email}
-          </li>
-        ))}
-      </ol>
+      <UserAuth />
     </div>
   );
 }
