6132ce99ab76b866c91c77aa0cff2cea2288630b
Giao Vu Dinh <tvudinh20@gmail.com>
2025-12-25 00:22:49 +0700

    homepage update

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import Loader from "@/components/ui/Loader";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 import styles from "./HomePage.module.css";
 
 export default function HomePage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSignIn = async () => {
+    setIsLoading(true);
+    await signIn("github");
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className={styles.container}>
-      <p className={styles.text}>Get Started</p>
+      <h1>visual notetaking made easy.</h1>
+      <button onClick={handleSignIn} className={styles.button}>
+        Get Started
+      </button>
     </div>
   );
 }
-
